8d0fb166b8062be60f031ef5c0f65206d0be3903
bbaalee <valerian215@gmail.com>
2023-11-15 16:31:25 +0900

    지영+별

--- spring231110/src/main/java/com/myweb/www/controller/HomeController.java
+++ spring231110/src/main/java/com/myweb/www/controller/HomeController.java
@@ -26,7 +26,6 @@ public class HomeController {
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
-		logger.info("dddldldldl");
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
@@ -34,7 +33,7 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
-		return "/member/login";
+		return "index";
 	}
 	
 }
