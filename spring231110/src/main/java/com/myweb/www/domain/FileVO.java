6cac8c5df996ec7d8a9deef50a00769e8f4b26eb
bbaalee <valerian215@gmail.com>
2023-12-06 18:49:43 +0900

    no message

---
+++ spring231110/src/main/java/com/myweb/www/domain/FileVO.java
@@ -0,0 +1,24 @@
+package com.myweb.www.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class FileVO {
+	   private String uuid; 
+	   private String saveDir;
+	   private String fileName;
+	   private long fileSize;
+	   private String regAt;
+	   private int fileType;
+	   private String id;//프로필 사진 받는 용도
+	   private long pno;//포폴사진 받는 용도
+	   private long rno;//리뷰사진 받는 용도
+	}
---
+++ spring231110/src/main/java/com/myweb/www/security/MemberDTO.java
@@ -0,0 +1,17 @@
+package com.myweb.www.security;
+
+import lombok.Setter;
+import lombok.ToString;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberDTO {
+	private MemberVO mvo;
+	private String kakaoId;
+}
